9605f2f28dbbda5a1d2ef5baeca8435738d743db
Noah <noahglennmaldonado@gmail.com>
2024-01-29 01:04:37 -0500

    insalled MUI and created dropdown-menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,27 @@
 import { Link } from "react-router-dom";
+import ReorderIcon from "@mui/icons-material/Reorder";
 
 export function Navbar() {
   return (
-    <div className="navbar text-lg bg-red-500 text-yellow-500 h-20  flex items-center p-2">
-      <div className="leftSide mr-auto w-1/2 flex justify-around ">
-        <Link to="/">Home</Link>
-        <Link to="/menu">Menu</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
+    <div className=" navbar text-lg bg-red-500 text-yellow-500 h-20  flex items-center p-2">
+      <div className="leftSide mr-auto w-1/2 flex justify-start sm:justify-around">
+        <Link className="sm:block hidden" to="/">
+          Home
+        </Link>
+        <Link className="sm:block hidden" to="/menu">
+          Menu
+        </Link>
+        <Link className="sm:block hidden" to="/about">
+          About
+        </Link>
+        <Link className="sm:block hidden" to="/contact">
+          Contact
+        </Link>
+        <button className="menu-btn ">
+          <ReorderIcon />
+        </button>
       </div>
-      <div className="rightSide">
+      <div className="rightSide ml-auto">
         <button className="checkout-btn">Checkout</button>
       </div>
     </div>
