8d07b269a1a8a39debc929ca2cefeffd7750d134
Noah <noahglennmaldonado@gmail.com>
2024-02-22 09:58:05 -0500

    added new entries, updated css

--- src/data/MenuList.jsx
+++ src/data/MenuList.jsx
@@ -1,15 +1,39 @@
 import CheesePizza from "../assets/Cheese-pizza-1.webp";
-
+import Pepperoni from "../assets/Quick-and-Easy-Pepperoni-Pizza-700x700.webp";
+import Jalapeno from "../assets/Pizza-with-Jalapeno-Coppa-and-Hot-Honey.jpg";
+import Pineapple from "../assets/horizontal.avif";
+import Jumbo from "../assets/bolis4.jpg";
+import Chicago from "../assets/IMG_5452.0.jpeg";
 // import Cheese from
 export const MenuList = [
   {
     name: "Peperoni Pizza",
-    image: "",
+    image: Pepperoni,
     price: 9.99,
   },
   {
     name: "Cheese Pizza",
-    // image: CheesePizza,
+    image: CheesePizza,
     price: 9.99,
+  },
+  {
+    name: "Jalapeno Pizza",
+    image: Jalapeno,
+    price: 9.99,
+  },
+  {
+    name: "Pinaple Pizza",
+    image: Pineapple,
+    price: 9.99,
+  },
+  {
+    name: "Super Jumbo Pizza",
+    image: Jumbo,
+    price: 15.99,
+  },
+  {
+    name: "Chicago Style Pizza",
+    image: Chicago,
+    price: 15.99,
   },
 ];
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -5,10 +5,11 @@ export function Menu() {
   return (
     <div className="menu w-full h-auto flex justify-center items-center flex-col">
       <h1 className="menuTitle font-cursive text-[60px]">Our Menu</h1>
-      <div className="menuList w-[70vw] h-auto grid grid-cols-3 place-items-center">
+      <div className="menuList xl:grid-cols-3 md:grid-cols-2  w-[90vw] h-auto grid grid-cols-1 place-items-center">
         {MenuList.map((menuItem, key) => {
           return (
             <MenuItem
+              key={key}
               image={menuItem.image}
               name={menuItem.name}
               price={menuItem.price}
